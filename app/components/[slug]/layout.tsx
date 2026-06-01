b296a255c1178a5345c4e03ca13b8776e140d4fc
Alex Kostyniuk <kostyniuk7@gmail.com>
2026-06-01 22:54:36 +0200

    Remove sidebar from components index layout

---
+++ app/components/[slug]/layout.tsx
@@ -0,0 +1,12 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import { GlassSidebarInset } from "@/components/ui/glasscn/glass-sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
+
+export default function ComponentDocsLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <GlassSidebarInset className="bg-transparent">{children}</GlassSidebarInset>
+    </SidebarProvider>
+  );
+}
--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -1,12 +1,3 @@
-import { AppSidebar } from "@/components/app-sidebar";
-import { GlassSidebarInset } from "@/components/ui/glasscn/glass-sidebar";
-import { SidebarProvider } from "@/components/ui/sidebar";
-
 export default function ComponentsLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <SidebarProvider>
-      <AppSidebar />
-      <GlassSidebarInset className="bg-transparent">{children}</GlassSidebarInset>
-    </SidebarProvider>
-  );
+  return children;
 }
